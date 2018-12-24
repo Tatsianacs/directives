0ac8059965d48aa1aa7aee2feb5adaf0f1ee7632
tania <strekasia@tut.by>
2018-12-24 13:40:26 +0300

    feat: add directives

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1,3,5];
+  evenNumbers = [2,4];
+  onlyOdd = false;
+
+
+    heroes = [
+        {id: 1, name:'Superman'},
+        {id: 2, name:'Batman'},
+        {id: 5, name:'BatGirl'},
+        {id: 3, name:'Robin'},
+        {id: 4, name:'Flash'}
+    ];
+}
